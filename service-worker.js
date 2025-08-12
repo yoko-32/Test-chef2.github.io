afe1fa2204122cdc8431ab1bf66d9949da41be97
yoko-32 <yokomeontre@gmail.com>
2025-08-11 22:30:46 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,29 @@
-const CACHE="pepper-slayer-v2";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./service-worker.js","./assets/sprites/chef_placeholder_spritesheet.png"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
+const CACHE = "pepper-slayer-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./sprites/chef_placeholder_spritesheet.png",
+  "./audio/music.ogg",
+  "./audio/slash.wav",
+  "./audio/jump.wav",
+  "./audio/collect.wav"
+  // add any other js/css/images you use
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
